15393fab1fc6d38c8a4de8c0eefe8a6a49c1516d
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2022-01-13 20:35:07 +0530

    Updated aboutpage

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,18 @@ const AboutPage = () => {
             <Layout>
                 <Head title="About"/>
                 <header>About Me!</header>
-                <p>Hello, Aditya here. I am a Programmer, Art lover. I also try to write short stories sometimes when I am in the mood.I like to delve into many more interesting areas in the future.</p>
+                <p>Hello, Aditya here. I am a Programmer, Art lover, free thinker. I write short stories sometimes when I am in the mood.I like to delve into many more interesting areas in the future.</p>
+                <ul>
+                    <li>I like to learn things the hard way</li>
+                    <li>I am a hacker. I like to build things</li>
+                    <li>I love artistic movies. My top 3 favourite movies are
+                    <ul>
+                        <li>Blade Runner 2049</li>
+                        <li>The Social Network</li>
+                        <li>2001: A Space Odessey</li>
+                    </ul></li>
+                    <li>I am an avid Reader of Paul Graham Essays and Hacker News</li>
+                </ul>
             </Layout>
         </div>
     )
